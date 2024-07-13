76abbabed71f9d1ac9b1ec0cbe6887a00c78f33f
Hitesh <21amtics465@gmail.com>
2024-07-13 12:19:12 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDb ");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb ", err);
+  });
 
 app.listen(5173, () => {
   console.log("Server is running on port 5173");
